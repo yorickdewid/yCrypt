649b40b198b04b82a941c1d559cf80c5acd4faa5
yorickdewid <yorick17@outlook.com>
2016-05-18 13:12:38 +0200

    Decrypt file

--- yCrypt/yCrypt.cpp
+++ yCrypt/yCrypt.cpp
@@ -21,13 +21,17 @@ WCHAR szFile[MAX_PATH];								// Original file name
 
 // Forward declarations of functions included in this code module:
 INT_PTR CALLBACK	EncryptProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
+INT_PTR CALLBACK	DecryptProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
 
 typedef struct tagYENCFILE
 {
-	BYTE		cbSignature[MAX_SIGNATURE];				// yCrypt signature
-	BYTE		cbNonce[crypto_secretbox_NONCEBYTES];	// Cipher nonce
-	UINT        rawSize;								// Original filesize in bytes
-	UINT        algorithm;								// Algorithms used in this encryption
+	BYTE		cbSignature[MAX_SIGNATURE];										// yCrypt signature
+	BYTE		cbNonce[crypto_secretbox_NONCEBYTES];							// Cipher nonce
+	BYTE		cbPasswordSalt[crypto_pwhash_scryptsalsa208sha256_SALTBYTES];	// Password salt
+	UINT        rawSize;														// Original filesize in bytes
+	UINT        algorithm;														// Algorithms used in this encryption
+	DWORD		dwOps;															// CPU operations for password hashing
+	DWORD		dwMemory;														// Memory limit for password hashing
 } YENCFILE;
 
 LPCWCHAR fileNameExt(LPCWCHAR filename)
@@ -82,9 +86,9 @@ int APIENTRY wWinMain(HINSTANCE hInstance,
 	}
 
 	LPCWSTR szExt = fileNameExt(szFile);
-	if (szExt == L"yc") {
+	if (!wcscmp(szExt, L"yc")) {
 		// Decryption process
-		MessageBox(NULL, L"We should decrypt", L"Action", 0);
+		DialogBox(hInstance, MAKEINTRESOURCE(IDD_DECRYPT), NULL, DecryptProc);
 	} else {
 		// Encryption process
 		DialogBox(hInstance, MAKEINTRESOURCE(IDD_ENCRYPT), NULL, EncryptProc);
@@ -165,6 +169,7 @@ BOOL SodiumEncryptFile(LPTSTR password)
 	// ENC
 	int nCiphersz = crypto_secretbox_MACBYTES + nFilesz;
 	HLOCAL hCipherBuffer = LocalAlloc(LPTR, nCiphersz * sizeof(BYTE));
+	// handle NULL
 
 	BYTE nonce[crypto_secretbox_NONCEBYTES];
 
@@ -174,20 +179,23 @@ BOOL SodiumEncryptFile(LPTSTR password)
 	YENCFILE encFileStrct;
 	memcpy(encFileStrct.cbSignature, pszSignature, MAX_SIGNATURE);
 	memcpy(encFileStrct.cbNonce, nonce, crypto_secretbox_NONCEBYTES);
+	memcpy(encFileStrct.cbPasswordSalt, salt, crypto_pwhash_scryptsalsa208sha256_SALTBYTES);
 	encFileStrct.rawSize = nFilesz;
 	encFileStrct.algorithm = YC_SCRYPT_SALSA_POLY;
+	encFileStrct.dwOps = crypto_pwhash_scryptsalsa208sha256_OPSLIMIT_INTERACTIVE;
+	encFileStrct.dwMemory = crypto_pwhash_scryptsalsa208sha256_MEMLIMIT_INTERACTIVE;
 
 	wcscat_s(szFile, MAX_PATH, L".yc");
 
 	// WRITE
 	HANDLE hFileEncrypt = CreateFile(
-		szFile,					// name of the write
-		GENERIC_WRITE,			// open for writing
-		0,						// do not share
-		NULL,					// default security
-		CREATE_NEW,				// create new file only
-		FILE_ATTRIBUTE_NORMAL,	// normal file
-		NULL);					// no attr. template
+		szFile,					// Name of the write
+		GENERIC_WRITE,			// Open for writing
+		0,						// Do not share
+		NULL,					// Default security
+		CREATE_NEW,				// Create new file only
+		FILE_ATTRIBUTE_NORMAL,	// Normal file
+		NULL);					// No attr. template
 
 	if (hFileEncrypt == INVALID_HANDLE_VALUE)
 	{
@@ -197,11 +205,11 @@ BOOL SodiumEncryptFile(LPTSTR password)
 	}
 
 	bErrorFlag = WriteFile(
-		hFileEncrypt,			// open file handle
-		&encFileStrct,			// start of data to write
-		sizeof(YENCFILE),		// number of bytes to write
-		&numread,				// number of bytes that were written
-		NULL);					// no overlapped structure'
+		hFileEncrypt,			// Open file handle
+		&encFileStrct,			// Start of data to write
+		sizeof(YENCFILE),		// Number of bytes to write
+		&numread,				// Number of bytes that were written
+		NULL);					// No overlapped structure'
 
 	if (!bErrorFlag)
 	{
@@ -210,11 +218,11 @@ BOOL SodiumEncryptFile(LPTSTR password)
 	}
 
 	bErrorFlag = WriteFile(
-		hFileEncrypt,			// open file handle
-		hCipherBuffer,			// start of data to write
-		nCiphersz,				// number of bytes to write
-		&numread,				// number of bytes that were written
-		NULL);					// no overlapped structure
+		hFileEncrypt,			// Open file handle
+		hCipherBuffer,			// Start of data to write
+		nCiphersz,				// Number of bytes to write
+		&numread,				// Number of bytes that were written
+		NULL);					// No overlapped structure
 
 	if (!bErrorFlag)
 	{
@@ -228,23 +236,110 @@ exit_on_failure:
 	// Override sensitive in memory
 	sodium_memzero(password, wcslen(password) * sizeof(TCHAR));
 	sodium_memzero(c_szPassword, strlen(c_szPassword) * sizeof(BYTE));
-	sodium_memzero(key, sizeof(key));
+	sodium_memzero(key, crypto_secretbox_KEYBYTES);
 
 	LocalFree(hFileBuffer);
 	LocalFree(hCipherBuffer);
 
 	return bErrorFlag;
-	/*
-	unsigned char *szDecBuffer = (unsigned char *)calloc(nFilesz + 1, sizeof(char));
-	if (!szDecBuffer) {
+}
+
+
+BOOL SodiumDecryptFile(LPTSTR password)
+{
+	ULONG numread;
+	BOOL bErrorFlag;
+	YENCFILE encFileStrct;
+	int nFilesz;
+
+	if (!ReadFile(hFile, &encFileStrct, sizeof(YENCFILE), &numread, NULL)) {
+		MessageBox(NULL, L"Cannot read file into memory", L"Decryption error", MB_ICONERROR);
+
+		bErrorFlag = FALSE;
+		// goto exit_on_failure;
+	}
+
+	if (sodium_memcmp(encFileStrct.cbSignature, pszSignature, MAX_SIGNATURE))
+	{
+		MessageBox(NULL, L"This is not an encrypted file", L"Decryption error", MB_ICONERROR);
+		bErrorFlag = FALSE;
+	}
+
+	HLOCAL hCipherBuffer = LocalAlloc(LPTR, (encFileStrct.rawSize + crypto_secretbox_MACBYTES) * sizeof(BYTE));
+	if (!hCipherBuffer) {
+		MessageBox(NULL, L"Cannot allocate memory", L"Decryption error", MB_ICONERROR);
+
+		bErrorFlag = FALSE;
+		// goto exit_on_failure;
+	}
+
+	if (!ReadFile(hFile, hCipherBuffer, encFileStrct.rawSize + crypto_secretbox_MACBYTES, &numread, NULL)) {
+		MessageBox(NULL, L"Cannot read file into memory", L"Decryption error", MB_ICONERROR);
+
+		bErrorFlag = FALSE;
+		// goto exit_on_failure;
+	}
+
+	// For now read byes must equal filesize
+	assert((encFileStrct.rawSize + crypto_secretbox_MACBYTES) == numread);
+
+	CloseHandle(hFile);
+
+	// Wide password to multibyte password
+	size_t nConvChars;
+	char c_szPassword[100]; // This might not be enough
+	wcstombs_s(&nConvChars, c_szPassword, password, wcslen(password) + 1);
+
+	// DERIVE KEY
+	BYTE key[crypto_secretbox_KEYBYTES];
+
+	if (crypto_pwhash_scryptsalsa208sha256(key, crypto_secretbox_KEYBYTES, c_szPassword, strlen(c_szPassword), encFileStrct.cbPasswordSalt,
+		encFileStrct.dwOps,
+		encFileStrct.dwMemory) != 0) {
+
+		MessageBox(NULL, L"Operation takes too much system resources", L"Decryption error", MB_ICONERROR);
+
+		bErrorFlag = FALSE;
+		// goto exit_on_failure;
+	}
+
+	HLOCAL hDecBuffer = LocalAlloc(LPTR, encFileStrct.rawSize * sizeof(char));
+	if (!hDecBuffer) {
 		return FALSE;
 	}
 
-	if (crypto_secretbox_open_easy(szDecBuffer, szCipherBuffer, nCiphersz, nonce, key) != 0) {
-		// Ouch
-	}*/
-}
+	if (crypto_secretbox_open_easy((LPBYTE)hDecBuffer, (LPBYTE)hCipherBuffer, encFileStrct.rawSize + crypto_secretbox_MACBYTES, encFileStrct.cbNonce, key) != 0) {
+		MessageBox(NULL, L"Decryption failed, please try again", L"Decryption error", MB_ICONERROR);
+		return FALSE;
+	}
 
+	// WRITE
+	HANDLE hFileEncrypt = CreateFile(
+		L"kaas.txt",			// name of the write
+		GENERIC_WRITE,			// open for writing
+		0,						// do not share
+		NULL,					// default security
+		CREATE_NEW,				// create new file only
+		FILE_ATTRIBUTE_NORMAL,	// normal file
+		NULL);					// no attr. template
+
+	if (hFileEncrypt == INVALID_HANDLE_VALUE) {
+		MessageBox(NULL, L"Cannot open file", L"Decryption error", MB_ICONERROR);
+		bErrorFlag = FALSE;
+		// goto exit_on_failure;
+	}
+
+	bErrorFlag = WriteFile(
+		hFileEncrypt,			// open file handle
+		hDecBuffer,			// start of data to write
+		encFileStrct.rawSize,	// number of bytes to write
+		&numread,				// number of bytes that were written
+		NULL);					// no overlapped structure'
+
+	CloseHandle(hFileEncrypt);
+
+	return TRUE;
+}
 
 // Message handler for about box.
 INT_PTR CALLBACK EncryptProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
@@ -304,13 +399,18 @@ INT_PTR CALLBACK EncryptProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lPar
 				break;
 			}
 
-			EndDialog(hDlg, LOWORD(wParam));
-
 			if (!SodiumEncryptFile(pwd1))
 			{
-				PostQuitMessage(1);
+				sodium_memzero(pwd1, MAX_PASSWORD);
+				sodium_memzero(pwd2, MAX_PASSWORD);
+				
+				break;
 			}
 			
+			sodium_memzero(pwd1, MAX_PASSWORD);
+			sodium_memzero(pwd2, MAX_PASSWORD);
+
+			EndDialog(hDlg, LOWORD(wParam));
 			PostQuitMessage(0);
 			return (INT_PTR)TRUE;
 
@@ -323,5 +423,80 @@ INT_PTR CALLBACK EncryptProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lPar
 		break;
 	}
 	
+	return (INT_PTR)FALSE;
+}
+
+
+// Message handler for about box.
+INT_PTR CALLBACK DecryptProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+
+	switch (message) {
+	case WM_INITDIALOG:
+		// Get the owner window and dialog box rectangles. 
+		HWND hwndOwner;
+		RECT rc, rcDlg, rcOwner;
+
+		if ((hwndOwner = GetParent(hDlg)) == NULL) {
+			hwndOwner = GetDesktopWindow();
+		}
+
+		GetWindowRect(hwndOwner, &rcOwner);
+		GetWindowRect(hDlg, &rcDlg);
+		CopyRect(&rc, &rcOwner);
+
+		// Offset the owner and dialog box rectangles so that right and bottom 
+		// values represent the width and height, and then offset the owner again 
+		// to discard space taken up by the dialog box. 
+
+		OffsetRect(&rcDlg, -rcDlg.left, -rcDlg.top);
+		OffsetRect(&rc, -rc.left, -rc.top);
+		OffsetRect(&rc, -rcDlg.right, -rcDlg.bottom);
+
+		// The new position is the sum of half the remaining space and the owner's 
+		// original position. 
+
+		SetWindowPos(hDlg,
+			HWND_TOP,
+			rcOwner.left + (rc.right / 2),
+			rcOwner.top + (rc.bottom / 2),
+			0, 0,          // Ignores size arguments. 
+			SWP_NOSIZE);
+
+		SetWindowText(hDlg, szFile);
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK) {
+			TCHAR pwd[MAX_PASSWORD];
+
+			GetDlgItemText(hDlg, IDC_PWD, pwd, MAX_PASSWORD);
+
+			if (!wcslen(pwd)) {
+				MessageBox(hDlg, L"Password cannot be empty", L"Password error", MB_ICONERROR);
+				break;
+			}
+
+			if (!SodiumDecryptFile(pwd)) {
+				sodium_memzero(pwd, MAX_PASSWORD);
+				break;
+			}
+
+			sodium_memzero(pwd, MAX_PASSWORD);
+
+			EndDialog(hDlg, LOWORD(wParam));
+			PostQuitMessage(0);
+			return (INT_PTR)TRUE;
+
+		} else if (LOWORD(wParam) == IDCANCEL) {
+			EndDialog(hDlg, LOWORD(wParam));
+			PostQuitMessage(0);
+			return (INT_PTR)TRUE;
+
+		}
+		break;
+	}
+
 	return (INT_PTR)FALSE;
 }
